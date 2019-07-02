1918140d0de7c7a228e8dab6f3e542b302009b4e
ShK1peR-69 <grigorich.poker@yandex.ru>
2019-07-02 17:03:38 +0300

    Added ExecutorService for mailing method

--- src/main/java/com/itis/master/practice/simpleapp/util/Mailing.java
+++ src/main/java/com/itis/master/practice/simpleapp/util/Mailing.java
@@ -5,6 +5,9 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /*****
  * @author Igor Astafyev
  * June, 2019
@@ -20,19 +23,23 @@ public class Mailing {
         this.mailSender = mailSender;
     }
 
-    public void sendMail(String email, String name) { // executor service, thread
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(email);
-        message.setFrom("springbootigoryan@yandex.ru");
-        message.setSubject("Подтверждение регистрации");
-        String key_one = DigestUtils.md5Hex(email);
-        String key_two = DigestUtils.md5Hex(key_one);
-        String text = "Здравствуйте, " + name + "! \n\n" +
-                "Для завершения регистрации Вам необходимо перейти по сслыке ниже: \n" +
-                "http://localhost:8080/registration/?mucho=" + key_one + "&rzd=" + key_two + "\n\n" +
-                "Если данное письмо отпрвлено Вам по ошибке, пожалуйста, проигнорируйте его.\n" +
-                "Спасибо!";
-        message.setText(text);
-        mailSender.send(message);
+    public void sendMail(String email, String name) {
+        ExecutorService service = Executors.newCachedThreadPool();
+        service.submit(() -> {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(email);
+            message.setFrom("springbootigoryan@yandex.ru");
+            message.setSubject("Подтверждение регистрации");
+            String key_one = DigestUtils.md5Hex(email);
+            String key_two = DigestUtils.md5Hex(key_one);
+            String text = "Здравствуйте, " + name + "! \n\n" +
+                    "Для завершения регистрации Вам необходимо перейти по сслыке ниже: \n" +
+                    "http://localhost:8080/registration/?mucho=" + key_one + "&rzd=" + key_two + "\n\n" +
+                    "Если данное письмо отпрвлено Вам по ошибке, пожалуйста, проигнорируйте его.\n" +
+                    "Спасибо!";
+            message.setText(text);
+            mailSender.send(message);
+        });
+        service.shutdown();
     }
 }
